f21351d31f10f7aa585ab8362cac136f04db981f
Anders429 <anders.stefan.evensen@gmail.com>
2022-12-29 13:43:22 -0800

    Test de::Error formatting,

--- src/de.rs
+++ src/de.rs
@@ -1,7 +1,7 @@
 use crate::{Token, Tokens};
-use alloc::vec;
+use alloc::{string::{String, ToString}, vec};
 use core::{fmt, fmt::Display};
-use serde::de;
+use serde::{de, de::{Expected, Unexpected}};
 use serde::de::Error as _;
 
 #[derive(Debug)]
@@ -9,6 +9,7 @@ pub struct Deserializer {
     tokens: vec::IntoIter<Token>,
 
     is_human_readable: bool,
+    self_describing: bool,
 }
 
 impl<'a, 'de> de::Deserializer<'de> for &'a mut Deserializer {
@@ -18,6 +19,9 @@ impl<'a, 'de> de::Deserializer<'de> for &'a mut Deserializer {
     where
         V: de::Visitor<'de>,
     {
+        if !self.self_describing {
+            return Err(Error::NotSelfDescribing);
+        }
         let token = self.next_token()?;
         match token {
             Token::Bool(v) => visitor.visit_bool(v),
@@ -417,6 +421,7 @@ pub struct Builder {
     tokens: Option<Tokens>,
 
     is_human_readable: Option<bool>,
+    self_describing: Option<bool>,
 }
 
 impl Builder {
@@ -430,6 +435,11 @@ impl Builder {
         self
     }
 
+    pub fn self_describing(&mut self, self_describing: bool) -> &mut Self {
+        self.self_describing = Some(self_describing);
+        self
+    }
+
     pub fn build(&mut self) -> Deserializer {
         Deserializer {
             tokens: self
@@ -440,6 +450,7 @@ impl Builder {
                 .into_iter(),
 
             is_human_readable: self.is_human_readable.unwrap_or(true),
+            self_describing: self.self_describing.unwrap_or(true),
         }
     }
 }
@@ -447,11 +458,33 @@ impl Builder {
 #[derive(Debug)]
 pub enum Error {
     EndOfTokens,
+
+    NotSelfDescribing,
+
+    Custom(String),
+    InvalidType(String, String),
+    InvalidValue(String, String),
+    InvalidLength(usize, String),
+    UnknownVariant(String, &'static [&'static str]),
+    UnknownField(String, &'static [&'static str]),
+    MissingField(&'static str),
+    DuplicateField(&'static str),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
+        match self {
+            Self::EndOfTokens => f.write_str("end of tokens"),
+            Self::NotSelfDescribing => f.write_str("attempted to deserialize as self-describing when deserializer is not set as self-describing"),
+            Self::Custom(s) => f.write_str(s),
+            Self::InvalidType(unexpected, expected) => write!(f, "invalid type: expected {}, found {}", expected, unexpected),
+            Self::InvalidValue(unexpected, expected) => write!(f, "invalid value: expected {}, found {}", expected, unexpected),
+            Self::InvalidLength(length, expected) => write!(f, "invalid length {}, expected {}", length, expected),
+            Self::UnknownVariant(variant, expected) => write!(f, "unknown variant {}, expected one of {:?}", variant, expected),
+            Self::UnknownField(field, expected) => write!(f, "unknown field {}, expected one of {:?}", field, expected),
+            Self::MissingField(field) => write!(f, "missing field {}", field),
+            Self::DuplicateField(field) => write!(f, "duplicate field {}", field),
+        }
     }
 }
 
@@ -462,6 +495,92 @@ impl de::Error for Error {
     where
         T: fmt::Display,
     {
-        todo!()
+        Self::Custom(msg.to_string())
+    }
+
+    fn invalid_type(unexpected: Unexpected, expected: &dyn Expected) -> Self {
+        Self::InvalidType(unexpected.to_string(), expected.to_string())
+    }
+
+    fn invalid_value(unexpected: Unexpected, expected: &dyn Expected) -> Self {
+        Self::InvalidValue(unexpected.to_string(), expected.to_string())
+    }
+
+    fn invalid_length(len: usize, expected: &dyn Expected) -> Self {
+        Self::InvalidLength(len, expected.to_string())
+    }
+
+    fn unknown_variant(variant: &str, expected: &'static [&'static str]) -> Self {
+        Self::UnknownVariant(variant.to_string(), expected)
+    }
+
+    fn unknown_field(field: &str, expected: &'static [&'static str]) -> Self {
+        Self::UnknownField(field.to_string(), expected)
+    }
+
+    fn missing_field(field: &'static str) -> Self {
+        Self::MissingField(field)
+    }
+
+    fn duplicate_field(field: &'static str) -> Self {
+        Self::DuplicateField(field)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Token;
+    use super::Error;
+    use alloc::{borrow::ToOwned, format};
+    use serde::de::Error as _;
+
+    #[test]
+    fn display_error_end_of_tokens() {
+        assert_eq!(format!("{}", Error::EndOfTokens), "end of tokens");
+    }
+
+    #[test]
+    fn display_error_not_self_describing() {
+        assert_eq!(format!("{}", Error::NotSelfDescribing), "attempted to deserialize as self-describing when deserializer is not set as self-describing");
+    }
+
+    #[test]
+    fn display_error_custom() {
+        assert_eq!(format!("{}", Error::Custom("foo".to_owned())), "foo");
+    }
+
+    #[test]
+    fn display_error_invalid_type() {
+        assert_eq!(format!("{}", Error::invalid_type((&Token::Bool(true)).into(), &"foo")), "invalid type: expected foo, found boolean `true`");
+    }
+
+    #[test]
+    fn display_error_invalid_value() {
+        assert_eq!(format!("{}", Error::invalid_value((&Token::Bool(true)).into(), &"foo")), "invalid value: expected foo, found boolean `true`");
+    }
+
+    #[test]
+    fn display_error_invalid_length() {
+        assert_eq!(format!("{}", Error::invalid_length(42, &"foo")), "invalid length 42, expected foo");
+    }
+
+    #[test]
+    fn display_error_unknown_variant() {
+        assert_eq!(format!("{}", Error::unknown_variant("foo", &["bar", "baz"])), "unknown variant foo, expected one of [\"bar\", \"baz\"]");
+    }
+
+    #[test]
+    fn display_error_unknown_field() {
+        assert_eq!(format!("{}", Error::unknown_field("foo", &["bar", "baz"])), "unknown field foo, expected one of [\"bar\", \"baz\"]");
+    }
+
+    #[test]
+    fn display_error_missing_field() {
+        assert_eq!(format!("{}", Error::missing_field("foo")), "missing field foo");
+    }
+
+    #[test]
+    fn display_error_duplicate_field() {
+        assert_eq!(format!("{}", Error::duplicate_field("foo")), "duplicate field foo");
     }
 }
